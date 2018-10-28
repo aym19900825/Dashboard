09d0fd5d14d17500953564662775dcff4ab22b16
aym <825185228@qq.com>
2018-10-28 22:11:41 +0800

    用户角色配置

--- src/page/sys/user/user.vue
+++ src/page/sys/user/user.vue
@@ -27,7 +27,7 @@
           <el-table-column label="操作" width="260">
             <template slot-scope="scope">
               <el-button size="mini" type="danger" @click="del(scope.$index, scope.row)">删除</el-button>
-              <el-button size="mini" type="success" @click="setRole(scope.$index, scope.row)" style="margin-left: 10px;margin-right: 10px;">配置角色</el-button>
+              <el-button size="mini" type="success" @click="set(scope.$index, scope.row)" style="margin-left: 10px;margin-right: 10px;">配置角色</el-button>
               <el-button size="mini" type="success" @click="edit(scope.$index, scope.row)" style="margin-left: 10px;margin-right: 10px;">编辑</el-button>
             </template>
           </el-table-column>
@@ -76,8 +76,31 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="resetUser">取 消</el-button>
-        <el-button type="primary" @click="add">确 定</el-button>
-        <el-button type="primary" @click="saveEdit">确 定</el-button>
+        <el-button type="primary" @click="add" v-show="!isEditUser">确 定</el-button>
+        <el-button type="primary" @click="saveEdit" v-show="isEditUser">确 定</el-button>
+      </div>
+    </el-dialog>
+    <el-dialog title="用户角色信息" :visible.sync="roleDailog"  width="500px" :before-close="resetRole">
+      <el-form :model="userRole"  label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="userRole.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="配置角色">
+         <el-select
+           v-model="userroles" multiple filterable default-first-option placeholder="请配置用户角色">
+           <el-option
+             v-for="item in rolelist"
+             :key="item.roleid"
+             :label="item.roledesc"
+             :value="item.roleid"
+             >
+           </el-option>
+         </el-select>
+       </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="resetRole">取 消</el-button>
+        <el-button type="primary" @click="saveSet">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -95,9 +118,14 @@ export default {
         totalCount: 0,
       },
       list: [],
+
+      isSet: false,
+      roleDailog: true,
+
       userDailog: false,
       isEditUser: false,
       editUserId: '',
+
       user: {
         name: "",
         address: "",
@@ -110,15 +138,17 @@ export default {
         department: "",
         userroles: []
       },
-      // roles: [
-      //   {
-      //     "roleid": 3,
-      //     "roledesc": "管理员hk1",
-      //     "rolename": "adminhk1",
-      //     "rolestatus": null,
-      //     "sortnum": null,
-      //   }
-      // ]
+
+      userRole: {
+        username: '',
+        userroles: []
+      },
+      rolelist: [
+        // 'roleid': 3,
+        // 'roledesc': "管理员hk1",
+        // 'rolename': "adminhk1",
+        // 'rolestatus': null,
+      ]
     }
   },
   methods: {
@@ -154,6 +184,9 @@ export default {
               showClose: true
           })
       })
+    },
+    set(){
+      this.isSet = true;
     },
     showUser(){
       this.userDailog = true;
@@ -255,6 +288,75 @@ export default {
           })
       })
     },
+    showSet(index, row){
+      var url = '/api/system/userRoleList';
+      this.$axios.put(url,{
+        integerId: row.userid
+      }).then((res) => {
+         if(res.data.code != 1){
+            this.$message({
+                type: 'error',
+                message: '网络错误，请重试',
+                showClose: true
+            })
+         }else{
+            this.roleDailog = true;
+            this.userRole.userroles = res.data;
+         }
+      }).catch((err) => {
+          this.$message({
+              type: 'error',
+              message: '网络错误，请重试',
+              showClose: true
+          })
+      })
+    },
+    saveSet(){
+      var url = '/api/system/userRoles';
+      this.$axios.put(url,{
+        userid: "1",
+        roleList: [
+          {
+            "userroleid":"",
+            "roleid":1,
+            "userid":1
+          },
+          {
+            "userroleid":"",
+            "roleid":3,
+            "userid":1
+          }
+        ]
+      }).then((res) => {
+         if(res.data.code != 1){
+            this.$message({
+                type: 'error',
+                message: '网络错误，请重试',
+                showClose: true
+            })
+         }else{
+            this.$message({
+              type: 'success',
+              message: '配置成功！',
+              showClose: true
+            });
+            this.resetRole();
+         }
+      }).catch((err) => {
+          this.$message({
+              type: 'error',
+              message: '网络错误，请重试',
+              showClose: true
+          })
+      })
+    },
+    resetRole(){
+      this.userRole = {
+        username: '',
+        userroles: []
+      };
+      this.roleDailog = false;
+    },
     resetUser(){
       this.user = {
         name: "",
@@ -270,6 +372,20 @@ export default {
       };
       this.userDailog = false;
       this.isEditUser = false;
+    },
+    getRoleList(){
+      var url = '/api/system/roleList?page=0&size=10';
+      this.$axios.post(url,{}).then((res) => {
+          if(res.data.code == 1){
+            this.rolelist = res.data.userList;
+          }
+      }).catch((err) => {
+          this.$message({
+              type: 'error',
+              message: '网络错误，请重试',
+              showClose: true
+          })
+      })
     },
     requestData(){
       var _this = this;
@@ -308,6 +424,7 @@ export default {
   },
   mounted(){
     this.requestData();
+    this.getRoleList();
   }
 }
 </script>
