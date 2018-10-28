2f4b8b4f8c801413ee4338c3f9e505125d8b2357
aym <825185228@qq.com>
2018-10-28 23:21:44 +0800

    模块dashboard配置

--- src/page/sys/model/model.vue
+++ src/page/sys/model/model.vue
@@ -20,7 +20,7 @@
           <el-table-column label="操作" width="260">
             <template slot-scope="scope">
               <el-button size="mini" type="danger" @click="del(scope.$index, scope.row)">删除</el-button>
-              <el-button size="mini" type="success" @click="set(scope.$index, scope.row)" style="margin-left: 10px;margin-right: 10px;">配置dashboard</el-button>
+              <el-button size="mini" type="success" @click="showSet(scope.$index, scope.row)" style="margin-left: 10px;margin-right: 10px;">配置dashboard</el-button>
               <el-button size="mini" type="success" @click="edit(scope.$index, scope.row)" style="margin-left: 10px;margin-right: 10px;">编辑</el-button>
             </template>
           </el-table-column>
@@ -53,26 +53,26 @@
         <el-button type="primary" @click="saveEdit" v-show="isEdit">确 定</el-button>
       </div>
     </el-dialog>
-    <el-dialog title="用户角色信息" :visible.sync="roleDailog"  width="500px" :before-close="resetRole">
-      <el-form :model="userRole"  label-width="80px">
+    <el-dialog title="用户角色信息" :visible.sync="dashboardDailog"  width="500px" :before-close="resetDashboard">
+      <el-form :model="modelDb"  label-width="80px">
         <el-form-item label="用户名">
-          <el-input v-model="userRole.username" autocomplete="off"></el-input>
+          <el-input v-model="modelDb.name" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="配置角色">
          <el-select
-           v-model="userroles" multiple filterable default-first-option placeholder="请配置用户角色">
+           v-model="modelDb.modelDbs" multiple filterable default-first-option placeholder="请配置用户角色">
            <el-option
-             v-for="item in rolelist"
-             :key="item.roleid"
-             :label="item.roledesc"
-             :value="item.roleid"
+             v-for="item in dashboardlist"
+             :key="item.dashboardid"
+             :label="item.dashboardname"
+             :value="item.dashboardid"
              >
            </el-option>
          </el-select>
        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="resetRole">取 消</el-button>
+        <el-button @click="resetDashboard">取 消</el-button>
         <el-button type="primary" @click="saveSet">确 定</el-button>
       </div>
     </el-dialog>
@@ -102,13 +102,13 @@ export default {
       },
 
       isSet: false,
-      roleDailog: true,
-      userRole: {
-        username: '',
-        userroles: []
+      dashboardDailog: true,
+      modelDb: {
+        name: '',
+        modelDbs: []
       },
 
-      rolelist: [
+      dashboardlist: [
         // 'roleid': 3,
         // 'roledesc': "管理员hk1",
         // 'rolename': "adminhk1",
@@ -252,9 +252,9 @@ export default {
       })
     },
     showSet(index, row){
-      var url = '/api/system/userRoleList';
+      var url = '/api/system/modelDashBoardlList';
       this.$axios.put(url,{
-        integerId: row.userid
+        integerId: row.modelid
       }).then((res) => {
          if(res.data.code != 1){
             this.$message({
@@ -263,8 +263,8 @@ export default {
                 showClose: true
             })
          }else{
-            this.roleDailog = true;
-            this.userRole.userroles = res.data;
+            this.dashboardDailog = true;
+            this.modelDb.modelDbs = res.data;
          }
       }).catch((err) => {
           this.$message({
@@ -276,20 +276,24 @@ export default {
     },
     saveSet(){
       var url = '/api/system/userRoles';
-      this.$axios.put(url,{
-        userid: "1",
-        roleList: [
-          {
-            "userroleid":"",
-            "roleid":1,
-            "userid":1
-          },
-          {
-            "userroleid":"",
-            "roleid":3,
-            "userid":1
-          }
-        ]
+      this.$axios.put(url, {
+        "modelid": 1,
+        "dashboardDtoList": [
+           {
+           "modeldashboardid":"",
+           "bid":3,
+           "modelid":1
+           },
+           {
+           "modeldashboardid":"",
+           "bid":6,
+           "modelid":1
+           },
+           {
+           "modeldashboardid":"",
+           "bid":7,
+           "modelid":1
+       }]
       }).then((res) => {
          if(res.data.code != 1){
             this.$message({
@@ -303,7 +307,8 @@ export default {
               message: '配置成功！',
               showClose: true
             });
-            this.resetRole();
+            this.resetDashboard();
+            this.requestData();
          }
       }).catch((err) => {
           this.$message({
@@ -313,12 +318,12 @@ export default {
           })
       })
     },
-    resetRole(){
-      this.userRole = {
-        username: '',
-        userroles: []
+    resetDashboard(){
+      this.dashboardDailog = false;
+      this.modelDb = {
+        name: '',
+        modelDbs: []
       };
-      this.roleDailog = false;
     },
     resetModel(){
       this.modelDailog = false;
@@ -327,20 +332,6 @@ export default {
         modelname: "",
         modeldesc: ""
       };
-    },
-    getRoleList(){
-      var url = '/api/system/roleList?page=0&size=10';
-      this.$axios.post(url,{}).then((res) => {
-          if(res.data.code == 1){
-            this.rolelist = res.data.userList;
-          }
-      }).catch((err) => {
-          this.$message({
-              type: 'error',
-              message: '网络错误，请重试',
-              showClose: true
-          })
-      })
     },
     requestData(){
       var _this = this;
@@ -360,13 +351,12 @@ export default {
     },
     getDashboards(){
       var _this = this;
-      var url = '/api/system/roleList?page=' + this.page.currentPage + '&' + this.page.pageSize;
+      var url = '/api/show/dashboardList';
       this.$axios.post(url,{
-        'name': 'admin',
-        'username': '管'
+        'page': 0,
+        'size': 10
       }).then((res) => {
-          this.list = res.data.userList;
-          this.page.totalCount = res.data.totalPages;
+           _this.dashboardlist = JSON.parse(JSON.stringify(res.data.visualizeList));
       }).catch((err) => {
           this.$message({
               type: 'error',
@@ -381,7 +371,7 @@ export default {
   },
   mounted(){
     this.requestData();
-    this.getRoleList();
+    this.getDashboards();
   }
 }
 </script>
